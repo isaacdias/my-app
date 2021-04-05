8e14153d8cee52aa33cd6e9f6fdf4da379c6360e
Isaac Dias <isaacardias@gmail.com>
2021-04-05 11:39:38 -0300

    criado projeto com express-generator

---
+++ controllers/usuariosController.js
@@ -0,0 +1,7 @@
+const usuariosController = {
+    index: (req, res) => {
+        return res.send('Estou exibindo uma mensagem atraves do controller!')
+    }
+}
+
+module.exports = usuariosController;
